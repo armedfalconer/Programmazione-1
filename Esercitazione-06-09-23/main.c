c4a4af6ea848c739d71cd593f737494df2b48a8c
armedfalconer <62673292+armedfalconer@users.noreply.github.com>
2024-01-18 14:26:46 +0100

    Update main.c

--- Esercitazione-06-09-23/main.c
+++ Esercitazione-06-09-23/main.c
@@ -1,1 +1,36 @@
+// compito 06/09/23
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "exam.h"
 
+int main(int argc, char** argv) {
+    
+    // Point A
+    Record par = decodeParameters(argc, argv);
+    puts("POINT A: ");
+    printRecord(par);
+    puts("");
+
+    // Point B
+    Array a = readInput(par.fileName);
+    puts("POINT B: ");
+    printArray(a);
+    puts("");
+
+    // POINT C
+    StackNode** P = initTop();
+    createStack(P, a, par.n);
+    puts("POINT C:");
+    printStack(P);
+    puts("");
+
+    // POINT D
+    float* values = findMinMaxMean(P);
+    printf("Min: %f\nMax: %f\nAverage: %f\n", values[0], values[1], values[2]);
+
+    freeStack(P);
+    free(values);
+
+    exit(EXIT_SUCCESS);
+}
